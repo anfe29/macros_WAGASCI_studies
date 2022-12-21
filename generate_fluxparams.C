0afa8397dc9b917d196a7649a7de419d9c6b8f6a
anfe <anfe29@github.com>
2022-12-21 10:28:06 +0100

    fixed binning

--- generate_fluxparams.C
+++ generate_fluxparams.C
@@ -1,6 +1,6 @@
 std::vector<double> enu = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 7.0, 10.0};
-std::vector<int> NeutrinoCode = {-14, 14};
-std::vector<int> isRHC = {-2,-1 , 0, 1};
+std::vector<int> NeutrinoCode = {14, -14};
+std::vector<int> isRHC = {-3, -1 , 0, 1, -2, -2};
 
 void generate_fluxparams()
 {
@@ -22,12 +22,12 @@ void generate_fluxparams()
 	
     //std::cout << "variables: SelectedSample NeutrinoCode isRHC Enu Enu\n";
     file << "variables: NeutrinoCode isRHC Enu Enu\n";
-	for(int i = 0; i < NeutrinoCode.size(); i++) {
+	for(int i = 0; i < isRHC.size(); i++) {
 		for(int j = 0; j < NeutrinoCode.size(); j++) {
             for(int k = 0; k < (enu.size() - 1); k++) {
                 //file << bins1[i] << " " << bins1[i+1] << "\n"; 
                 //std::cout << SelectedSample[i] << " " << NeutrinoCode[j] << " " << isRHC[k] << " " << enu[l] << " " << enu[l+1] << "\n"; 
-                file << NeutrinoCode[i] << " " << isRHC[j] << " " << enu[k] << " " << enu[k+1] << "\n"; 
+                file << isRHC[i] << " " << NeutrinoCode[j] << " " << enu[k] << " " << enu[k+1] << "\n"; 
             }
         }
     }
