32873829ce371af6a4490af5f3602cc50dbb951f
anfe29 <andres_munoz29@outlook.com>
2023-01-06 17:54:06 +0100

    new macros

--- make_gundamPlots.py
+++ make_gundamPlots.py
@@ -1,50 +1,27 @@
-import sys, getopt
-from ROOT import TFile, TH1, TGraph, TCanvas
+import sys
+from ROOT import TFile, TH1, TH2, TCanvas
 
-path = "studies_sampKenj/outputs/wagasci_sampKenj.root"
+# pass path to root file
+path = sys.argv[1] 
+print(sys.argv[1])
+# CAREFUL be sure to run from ../<macros-direcotry>, might implement some fix to this constraint later
 fname = path[25:-5]
+print(fname)
 
 infile = TFile(path)
-#outfile = TFile("plots/plots_"+fname+".root","recreate")
 
-infile.cd("FitterEngine/postFit/samples/canvas/D1True/reaction/")
-infile.ls()
-sample = infile.GetKey("sample_n0_TCanvas").ReadObj()
-sample.Draw()
-#samples.SaveAs(fname+"samples_n0.png")
-'''
-def getPlots(path):
-    # cut out file name out of the input path
-    #fname = path[8:-5]
-    path = "studies_sampKenj/outputs/wagasci_sampKenj.root"
-    fname = path[25:-5]
+# get postfit errors canvas
+gun_covmatrix = infile.Get("FitterEngine/postFit/Hesse/Hessian/postfitCovarianceOriginal_TH2D")
+gun_fitconst_flux = infile.Get("FitterEngine/postFit/Hesse/errors/Flux Systematics/values/fitConstraints_TCanvas")
+gun_fitconst_xsecfsi = infile.Get("FitterEngine/postFit/Hesse/errors/Cross-Section FSI Systematics/values/fitConstraints_TCanvas")
+gun_fitconst_xsec = infile.Get("FitterEngine/postFit/Hesse/errors/Cross-Section Systematics/values/fitConstraints_TCanvas")
+gun_fitconst_xsecbin = infile.Get("FitterEngine/postFit/Hesse/errors/Cross-Section (binned) Systematics/values/fitConstraints_TCanvas")
+gun_fitconst_xsecwg = infile.Get("FitterEngine/postFit/Hesse/errors/Cross-Section (WAGASCI-binned) Systematics/values/fitConstraints_TCanvas")
 
-    infile = TFile(path)
-    outfile = TFile("plots/"+fname+".root","recreate")
-
-def parse_args():
-    # intiate input variables form commande line
-    path = None 
-
-    # store arguments
-    argv = sys.argv[1:]
-    try:
-        opts, args = getopt.getopt(argv, "p:h:", ["path =",""])
-    except getopt.GetoptError as err:
-        print(err)
-        usage()
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt in ['-p', '--path']:
-            path = arg
-        if opt in ['-h', '--help']:
-            print("Macro used to extract plots from output GUNDAM files found in /outputs\n
-            Use the argument -p or --path and the path of the file you want to extract plots from.\n
-            Plots will be stored in the \plots directory.")
-
-    getPlots(path)
-
-# call function to parse input arguments
-parse_args()
-'''
+# save plots as png
+gun_covmatrix.SaveAs("plots/cov_matrix/" + fname + "covmatrix.png")
+gun_fitconst_flux.SaveAs("plots/fitconst/" + fname + "flux.png")
+gun_fitconst_xsecfsi.SaveAs("plots/fitconst/" + fname + "xsecfsi.png")
+gun_fitconst_xsec.SaveAs("plots/fitconst/" + fname + "xsec.png")
+gun_fitconst_xsecbin.SaveAs("plots/fitconst/" + fname + "xsecbin.png")
+gun_fitconst_xsecwg.SaveAs("plots/fitconst/" + fname + "xsecwg.png")
