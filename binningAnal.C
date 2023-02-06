07adca97db000e62c649f9b939aa53f8d09070db
anfe29 <andres_munoz29@outlook.com>
2023-02-06 17:54:23 +0100

    Update binning macros for analysis

--- binningAnal.C
+++ binningAnal.C
@@ -37,21 +37,21 @@ void binningAnal()
     int nsamp[SAMPLES] = {151, 152, 153, 154, 155, 157, 158};
 
     // define number of bins array 
-    int nbinscos[SAMPLES] = {5, 5, 5, 5, 5, 5, 5};
+    int nbinscos[SAMPLES] = {5, 5, 5, 5, 5, 5, 4};
 
     std::vector<std::vector<int>> nbinspmu = {
-        {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, 
+        {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12, 12}, {12, 12, 12, 12}, 
     };
 
     // define binning arrays
     std::vector<std::vector<double>> bincos = {
-        {0., 0.4, 0.5, 0.6 , 0.7 , 1}, 
-        {0., 0.9, 0.95, 0.98, 0.99, 1}, 
-        {0., 0.6 , 0.85 , 0.95, 0.99, 1}, 
-        {0., 0.5 , 0.6 , 0.7, 0.8, 1},
-        {0., 0.92, 0.96, 0.97, 0.99, 1},
-        {0., 0.65, 0.85, 0.9 , 0.95, 1},
-        {0., 0.6 , 0.85, 0.93, 0.98, 1} };
+        {0., 0.45, 0.5, 0.55, 0.65, 1}, 
+        {0., 0.9, 0.96, 0.98, 0.99, 1}, 
+        {0., 0.5 , 0.80 , 0.97, 0.99, 1}, 
+        {0., 0.6 , 0.65, 0.75, 0.85, 1},
+        {0., 0.90, 0.95, 0.98, 0.99, 1},
+        {0., 0.75, 0.87, 0.93, 0.97, 1},
+        {0., 0.7 , 0.87, 0.96, 1} };
 
     std::vector<std::vector<std::vector<double>>> binpmu = {
         {   //FHC WAGASCI PM-WMRD #nu_{#mu} CC 0#pi 
@@ -97,11 +97,11 @@ void binningAnal()
             {0, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 15000}
         },
         {   //FHC WAGASCI WG #nu_{#mu} CC 1#pi
-            {0, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 15000},
-            {0, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 15000},
-            {0, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 15000},
-            {0, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 15000},
-            {0, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 15000}
+            {0, 429, 450, 500, 525, 550, 575, 600, 650, 675, 700, 800, 15000},
+            {0, 425, 450, 475, 500, 525, 550, 600, 650, 700, 800, 900, 15000},
+            {0, 430, 500, 550, 600, 650, 700, 750, 800, 900, 1100, 1500, 15000},
+            {0, 420, 500, 600, 700, 800, 900, 1100, 1400, 1500, 1550, 1600, 15000},
+            {0, 422, 425, 700, 1000, 1450, 1500, 1600, 1800, 1825, 15000}
         },
     };
 
@@ -109,7 +109,7 @@ void binningAnal()
     std::vector<TH1D*> hcs;
     std::vector<TH1D*> hcs_res;
     std::vector<std::vector<TH1D*>> hpmu(SAMPLES);
-    std::vector<std::vector<TH1D*>> hpmu_res;
+    std::vector<std::vector<std::vector<TH1D*>>> hpmu_res(SAMPLES);
      
     // create histo instances
     for(int isample = 0; isample < SAMPLES; isample++) {
@@ -117,14 +117,14 @@ void binningAnal()
         // initialize histogram for costhetamu
         hcs.push_back(new TH1D("", "", nbinscos[isample], &bincos[isample][0]));
 
-        //hpmu_res.resize(nbinscos[isample]);
+        hpmu_res[isample].resize(nbinscos[isample]);
         // create hist for resolution and pmu per bin, then initialize them
        for(int i = 0; i < nbinscos[isample]; i++) {
             hpmu[isample].push_back(new TH1D("", "", nbinspmu[isample][i], &binpmu[isample][i][0]));
             hcs_res.push_back(new TH1D("", "", 100 , -1, 1));
-            //for(int j = 0; j < nbinspmu[isample][i]; j++) {
-                //hpmu_res[i].push_back(new TH1D("", "", 100 , 0, 15000));
-            //}
+            for(int j = 0; j < nbinspmu[isample][i]; j++) {
+                hpmu_res[isample][i].push_back(new TH1D("", "", 100 , 0, 15000));
+            }
         }
     }
 
@@ -132,7 +132,7 @@ void binningAnal()
     for(int ientry = 0; ientry < nevent; ientry++) {
         tree->GetEntry(ientry);
 
-        std::cout << ientry << "\n";
+        //std::cout << ientry << "\n";
         int isample = 0; 
         for(int n = 0; n < SAMPLES; n++) {
             if ( sample == nsamp[n] ) {
@@ -141,7 +141,7 @@ void binningAnal()
             }
         }
 
-        std::cout << isample << "\n";
+        //std::cout << isample << "\n";
         hcs[isample]->Fill(cs,peso);
         //hcs[isample]->Fill(cs);
         // fill resolution hist and pmu hist using bin edges
@@ -152,41 +152,60 @@ void binningAnal()
                 hcs_res[i]->Fill((cs-trucs)/trucs,peso);
                 //hpmu[i]->Fill(pmu,peso);
                 hpmu[isample][i]->Fill(pmu,peso);
-                //for(int j = 0; j < nbinspmu[isample][i]; j++) {
+                for(int j = 0; j < nbinspmu[isample][i]; j++) {
                     //std::cout << j << "\n";
-                    //if(pmu >= hpmu[isample][i]->GetXaxis()->GetBinLowEdge(j+1) && pmu < hpmu[isample][i]->GetXaxis()->GetBinUpEdge(j+1)) {
+                    if(pmu >= hpmu[isample][i]->GetXaxis()->GetBinLowEdge(j+1) && pmu < hpmu[isample][i]->GetXaxis()->GetBinUpEdge(j+1)) {
                         //std::cout << "Filling bin : [" << hpmu[isample][i]->GetXaxis()->GetBinLowEdge(j+1) << ", " << hpmu[isample][i]->GetXaxis()->GetBinUpEdge(j+1) << "] : " << pmu << "\n";
-                        //hpmu_res[i][j]->Fill((pmu-trupmu)/trupmu,peso);
-                    //}
-                //}
+                        hpmu_res[isample][i][j]->Fill((pmu-trupmu)/trupmu,peso);
+                    }
+                }
             }
         }
     }
 
+    // output 
     for(int isample = 0; isample < SAMPLES; isample++) {
 
         // when applying weights to histos use integral instead of gebincontents to get the right number of entries
-        std::cout << "\n\nFor " << nhist[isample] << " bin contents are:\nTotal entries in sample : " << hcs[isample]->Integral() << "\n";
+        std::cout << "\n\nSample :  " << nhist[isample] << " bin analysis :"
+        << "\nTotal entries in sample : " << hcs[isample]->Integral();
+
+        std::cout << std::endl << "********************************************************************" << std::endl;
+        //std::cout << std::endl << "Variable    "
+        std::cout << std::endl
+        << setw(12) << "Variable"       
+        //<< "Bin         "
+         << setw(14) << "Bin"
+        //<< "Events    "
+         << setw(10) << "Events"
+        //<< "Resolution     "
+         << setw(15) << "Resolution"    
+        //<< "Width     "
+         << setw(8) << "Width"
+        << setw(11) << "Bin Error " << std::endl;
+        std::cout << "********************************************************************";
 
         // Show bin contents
         for(int ibin = 1; ibin < hcs[isample]->GetNbinsX() + 1; ibin++) {
 
-            std::cout << "\nCosThetamu: Bin [" << hcs[isample]->GetXaxis()->GetBinLowEdge(ibin) << ", " << hcs[isample]->GetXaxis()->GetBinUpEdge(ibin);
-            std::cout << "] : " << hcs[isample]->GetBinContent(ibin) << "\t";
-            //std::cout << "] : " << hcs[isample]->Integral(ibin,ibin) << "\t";
-            // standard deviation is calculated with binned entries so underflow and overflows are not considered
-            std::cout << "Resolution : " << hcs_res[ibin-1]->GetStdDev()*2 << " \t";
-            std::cout << "Width : " << hcs[isample]->GetBinWidth(ibin) << "\t";
-            std::cout << "Error : " << hcs[isample]->GetBinError(ibin) << "\n";
+            std::cout << std::endl 
+            << setw(12) << "CosThetamu"
+            << " [" << setw(5)<< hcs[isample]->GetXaxis()->GetBinLowEdge(ibin) << "," << setw(5) << hcs[isample]->GetXaxis()->GetBinUpEdge(ibin) << "]" 
+            << setw(10) << hcs[isample]->GetBinContent(ibin)
+            << setw(15) << hcs_res[ibin-1]->GetStdDev()*2
+            << setw(8) << hcs[isample]->GetBinWidth(ibin)
+            << setw(11) << hcs[isample]->GetBinError(ibin) << std::endl;
 
             // show contents per bin and per sample for pmu
             for(int jbin = 1; jbin < hpmu[isample][ibin-1]->GetNbinsX() + 1; jbin++) {
-                std::cout << "\tPmu: Bin [" << hpmu[isample][ibin-1]->GetXaxis()->GetBinLowEdge(jbin) << ", " << hpmu[isample][ibin-1]->GetXaxis()->GetBinUpEdge(jbin);
-                std::cout << "] : " << hpmu[isample][ibin-1]->GetBinContent(jbin) << "\t";
-                std::cout << "Width : " << hpmu[isample][ibin-1]->GetBinWidth(jbin) << "\t";
-                std::cout << "Error : " << hpmu[isample][ibin-1]->GetBinError(jbin) << "\n";
-                //std::cout << "] : " << hpmu[ibin-1]->Integral() << "\t";
-                //std::cout << "Resolution : " << hpmu_res[ibin-1][jbin-1]->GetStdDev()*2 << " \n";
+
+                std::cout 
+                << setw(12) << "Pmu"
+                << " [" << setw(5) << hpmu[isample][ibin-1]->GetXaxis()->GetBinLowEdge(jbin) << "," << setw(5) << hpmu[isample][ibin-1]->GetXaxis()->GetBinUpEdge(jbin) << "]" 
+                << setw(10) << hpmu[isample][ibin-1]->GetBinContent(jbin)
+                << setw(15) << hpmu_res[isample][ibin-1][jbin-1]->GetStdDev()*2
+                << setw(8) << hpmu[isample][ibin-1]->GetBinWidth(jbin)
+                << setw(11) << hpmu[isample][ibin-1]->GetBinError(jbin) << std::endl;
             }
         }
     }
--- samplebinningWG.py
+++ samplebinningWG.py
@@ -1,181 +1,35 @@
-############################ UWG-BM CC-0pi #####################################
+import numpy as np
+
 # define bins
-bin1 = [0, 500, 700, 900, 1100, 1500, 30000]
-#bin1 = [0, 500, 600, 700, 750, 800, 1000, 1200, 1500]
-bin2  =[-1, 0.57, 0.71, 0.77, 0.82, 0.87, 0.91, 0.94, 0.97, 1]
-#bin2  =[0, 0.3, 0.8, 0.85, 0.9, 0.95, 1]
+#pmu = np.array([0, 500, 700, 900, 1100, 1500, 30000]) # KENJI
+#pmu = np.array([0 , 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 15000]) # 100s
+pmu = np.array([0, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1500, 15000]) # 50s
 
-# open output file 
-file = open("studies_sampKenj/inputs/samples/binning/WAGASCI_UWG-BM_CC-0pi.txt","w")
+#cos  =[-1, 0.57, 0.71, 0.77, 0.82, 0.87, 0.91, 0.94, 0.97, 1] # KENJI
+#cos  =[-1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.81, 0.9, 1] # 0.1s
+cos  =[-1, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.9, 0.95, 1] # 
 
-# write binning variables on first line of file
-file.write("variables: Pmu Pmu CosThetamu CosThetamu\n")
-#print("variables: Pmu Pmu")
+# sample name array
+samples = np.array(["WAGASCI_PM-WMRD_CC-0pi", "WAGASCI_PM-BM_CC-0pi",  "WAGASCI_PM_CC-1pi","WAGASCI_UWG-WMRD_CC-0pi", "WAGASCI_UWG-BM_CC-0pi", "WAGASCI_DWG-BM_CC-0pi", "WAGASCI_WG_CC-1pi"])
 
-# iterate over array to write binning
-for i in range(len(bin1) - 1):
-    for j in range(len(bin2) - 1):
-        line = str(bin1[i]) + " " + str(bin1[i+1]) + " " + str(bin2[j]) + " " + str(bin2[j+1]) + "\n" 
-        file.write(line) 
-    #print(bin1[i], bin1[i+1],sep=" ")
+for samp in range(samples.size):
+    # open output file 
+    file = open("studies_sampKenj/inputs/samples/binning/"+ samples[samp] +".txt","w")
+    #print("studies_sampKenj/inputs/samples/binning/"+ samples[samp] +".txt")
 
-# close output file and clear arrays
-file.close
-bin1.clear()
-bin2.clear()
+    # write binning variables on first line of file
+    file.write("variables: CosThetamu CosThetamu Pmu Pmu\n")
+    #file.write("variables: Pmu Pmu\n")
+    #print("variables: Pmu Pmu\n")
 
-########################### DWG-BM CC-0pi ######################################
-# define bins
-bin1 = [0, 500, 700, 900, 1100, 1500, 30000]
-#bin1 = [0, 375, 400, 425, 450, 475, 500, 550, 600, 650, 700, 800, 1000, 1200, 1500]
-bin2  =[-1, 0.57, 0.71, 0.77, 0.82, 0.87, 0.91, 0.94, 0.97, 1]
-#bin2  =[0, 0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
+    # iterate over array to write binning
+    #for i in range(len(pmu) - 1):
+    for i in range(len(cos) - 1):
+        for j in range(len(pmu) - 1):
+            #line = str(pmu[i]) + " " + str(pmu[i+1]) + "\n" 
+            line = str(cos[i]) + " " + str(cos[i+1]) + " " + str(pmu[j]) + " " + str(pmu[j+1]) + "\n" 
+            file.write(line) 
+            #print(line) 
 
-# open output file 
-file = open("studies_sampKenj/inputs/samples/binning/WAGASCI_DWG-BM_CC-0pi.txt","w")
-
-# write binning variables on first line of file
-file.write("variables: Pmu Pmu CosThetamu CosThetamu \n")
-#print("variables: Pmu Pmu")
-
-# iterate over array to write binning
-for i in range(len(bin1) - 1):
-    for j in range(len(bin2) - 1):
-        line = str(bin1[i]) + " " + str(bin1[i+1]) + " " + str(bin2[j]) + " " + str(bin2[j+1]) + "\n" 
-        file.write(line) 
-    #print(bin1[i], bin1[i+1],sep=" ")
-
-# close output file and clear arrays
-file.close
-bin1.clear()
-bin2.clear()
-
-########################### UWG-WMRD CC-0pi ###################################
-# define bins
-bin1 = [0, 500, 700, 900, 1100, 1500, 30000]
-#bin1 = [0, 400, 450, 500, 525, 575, 600, 700, 800, 1500]
-bin2  =[-1, 0.57, 0.71, 0.77, 0.82, 0.87, 0.91, 0.94, 0.97, 1]
-#bin2  =[0, 0.3, 0.5, 0.6, 0.7, 0.8, 1]
-
-# open output file 
-file = open("studies_sampKenj/inputs/samples/binning/WAGASCI_UWG-WMRD_CC-0pi.txt","w")
-
-# write binning variables on first line of file
-file.write("variables: Pmu Pmu CosThetamu CosThetamu \n")
-#print("variables: Pmu Pmu")
-
-# iterate over array to write binning
-for i in range(len(bin1) - 1):
-    for j in range(len(bin2) - 1):
-        line = str(bin1[i]) + " " + str(bin1[i+1]) + " " + str(bin2[j]) + " " + str(bin2[j+1]) + "\n" 
-        file.write(line) 
-    #print(bin1[i], bin1[i+1],sep=" ")
-
-# close output file and clear arrays
-file.close
-bin1.clear()
-bin2.clear()
-
-########################### PM-BM CC-0pi ######################################
-# define bins
-bin1 = [0, 500, 700, 900, 1100, 1500, 30000]
-#bin1 = [0, 400, 500, 550, 575, 600, 625, 650, 700, 750, 800, 850, 900, 1000, 1200, 1500]
-bin2  =[-1, 0.57, 0.71, 0.77, 0.82, 0.87, 0.91, 0.94, 0.97, 1]
-#bin2  =[0, 0.8, 0.85, 0.9, 0.925, 0.95, 0.975, 1]
-
-# open output file 
-file = open("studies_sampKenj/inputs/samples/binning/WAGASCI_PM-BM_CC-0pi.txt","w")
-
-# write binning variables on first line of file
-file.write("variables: Pmu Pmu CosThetamu CosThetamu \n")
-#print("variables: Pmu Pmu")
-
-# iterate over array to write binning
-for i in range(len(bin1) - 1):
-    for j in range(len(bin2) - 1):
-        line = str(bin1[i]) + " " + str(bin1[i+1]) + " " + str(bin2[j]) + " " + str(bin2[j+1]) + "\n" 
-        file.write(line) 
-    #print(bin1[i], bin1[i+1],sep=" ")
-
-# close output file and clear arrays
-file.close
-bin1.clear()
-bin2.clear()
-
-########################### PM-WMRD CC-0pi ####################################
-# define bins
-bin1 = [0, 500, 700, 900, 1100, 1500, 30000]
-#bin1 = [0, 300, 350, 375, 400, 425, 450, 475, 500, 550, 600, 650, 800, 1500]
-bin2  =[-1, 0.57, 0.71, 0.77, 0.82, 0.87, 0.91, 0.94, 0.97, 1]
-#bin2  =[0, 0.2, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 1]
-
-# open output file 
-file = open("studies_sampKenj/inputs/samples/binning/WAGASCI_PM-WMRD_CC-0pi.txt","w")
-
-# write binning variables on first line of file
-file.write("variables: Pmu Pmu CosThetamu CosThetamu \n")
-#print("variables: Pmu Pmu")
-
-# iterate over array to write binning
-for i in range(len(bin1) - 1):
-    for j in range(len(bin2) - 1):
-        line = str(bin1[i]) + " " + str(bin1[i+1]) + " " + str(bin2[j]) + " " + str(bin2[j+1]) + "\n" 
-        file.write(line) 
-    #print(bin1[i], bin1[i+1],sep=" ")
-
-# close output file and clear arrays
-file.close
-bin1.clear()
-bin2.clear()
-
-########################### PM CC-1pi #########################################
-# define bins
-bin1 = [0, 500, 700, 900, 1100, 1500, 30000]
-#bin1 = [0, 350, 400, 475, 450, 475, 500, 525, 550, 600, 650, 700, 800, 1000, 1200, 1500]
-bin2  =[-1, 0.57, 0.71, 0.77, 0.82, 0.87, 0.91, 0.94, 0.97, 1]
-#bin2  =[0, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.9, 0.95, 1]
-
-# open output file 
-file = open("studies_sampKenj/inputs/samples/binning/WAGASCI_PM_CC-1pi.txt","w")
-
-# write binning variables on first line of file
-file.write("variables: Pmu Pmu CosThetamu CosThetamu \n")
-#print("variables: Pmu Pmu")
-
-# iterate over array to write binning
-for i in range(len(bin1) - 1):
-    for j in range(len(bin2) - 1):
-        line = str(bin1[i]) + " " + str(bin1[i+1]) + " " + str(bin2[j]) + " " + str(bin2[j+1]) + "\n" 
-        file.write(line) 
-    #print(bin1[i], bin1[i+1],sep=" ")
-
-# close output file and clear arrays
-file.close
-bin1.clear()
-bin2.clear()
-
-########################### WG CC-1pi #########################################
-# define bins
-bin1 = [0, 500, 700, 900, 1100, 1500, 30000]
-#bin1 = [0, 375, 400, 450, 500, 550, 600, 700, 800, 1200, 1500]
-bin2  =[-1, 0.57, 0.71, 0.77, 0.82, 0.87, 0.91, 0.94, 0.97, 1]
-#bin2  =[0, 0.4, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1]
-
-# open output file 
-file = open("studies_sampKenj/inputs/samples/binning/WAGASCI_WG_CC-1pi.txt","w")
-
-# write binning variables on first line of file
-file.write("variables: Pmu Pmu CosThetamu CosThetamu \n")
-#print("variables: Pmu Pmu")
-
-# iterate over array to write binning
-for i in range(len(bin1) - 1):
-    for j in range(len(bin2) - 1):
-        line = str(bin1[i]) + " " + str(bin1[i+1]) + " " + str(bin2[j]) + " " + str(bin2[j+1]) + "\n" 
-        file.write(line) 
-    #print(bin1[i], bin1[i+1],sep=" ")
-
-# close output file and clear arrays
-file.close
-bin1.clear()
-bin2.clear()
+    # close output file 
+    file.close
