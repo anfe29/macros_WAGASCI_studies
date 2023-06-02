189013604a5b2ebd75f3b3e08d5d9de7401b11d1
anfe29 <andres_munoz29@outlook.com>
2023-06-02 17:32:42 +0900

    done with this macro

--- compare_gundam_andrew_postmat.py
+++ compare_gundam_andrew_postmat.py
@@ -8,56 +8,69 @@ gROOT.SetBatch(True)
 
 apath = "/sps/t2k/buizza/ND280/AndrewFitter/Wagasci/xsLLhFitter/outputs/xsecKenji_asimov_template-flux-det-xsec_noeigen_nosplinebug.root"
 gpath = "/sps/t2k/buizza/ND280/GUNDAM/feb2023/inputs_WAGASCIxsec/xsecKenji_test_template-flux-det-xsec_OPok_fakeSplineBinning_newTempPars.root"
-opath = "per_margherita/cov_posfit.root"
+#apath = "xsecKenji_asimov_template-flux-det-xsec_noeigen_nosplinebug.root"
+#gpath = "xsecKenji_test_template-flux-det-xsec_OPok_fakeSplineBinning_newTempPars.root"
+opath = "per_margherita/cov_postfit.root"
 
 if not os.path.exists(apath):
     apath = input("Enter xsllhFitter ouput file: ")
 if not os.path.exists(gpath):
     gpath = input("Enter GUDAM ouput file: ")
 
-# with uproot
-afile = uproot.open(apath)
-gfile = uproot.open(gpath)
-
-amatrix = afile["res_cov_matrix"].to_numpy()
-ghist = gfile["FitterEngine/postFit/Hesse/hessian/postfitCovarianceOriginal_TH2D"].to_numpy()
-
-# convert TH2D to Tmatrix
-gmatrix= np.array(ghist)
-
-omatrix = amatrix - gmatrix
-
-'''
 # with pyroot
 afile = TFile(apath)
 gfile = TFile(gpath)
 
-amatrix = afile.Get("res_cov_matrix")
+amatrix = TH2D(afile.Get("res_cov_matrix"))
 gmatrix = gfile.Get("FitterEngine/postFit/Hesse/hessian/postfitCovarianceOriginal_TH2D")
 
-if amatrix.GetNcols() != gmatrix.GetNbinsX():
+if amatrix.GetNbinsY() != gmatrix.GetNbinsY():
     print("Matrices not the same size!")
     exit()
 
-nbins = amatrix.GetNcols()
-omatrix = TH2D("cov_diff", "",nbinx )
+nbins = amatrix.GetNbinsX()
+omatrix = TH2D("diff_cov", "Difference between xsllhFitter and GUNDAM postfit matrices", nbins, 0, nbins, nbins, 0, nbins)
+omatrix1 = TH2D("reldiff_cov", "Relative Difference between xsllhFitter and GUNDAM postfit matrices", nbins, 0, nbins, nbins, 0, nbins)
 
 for ientry in range(nbins):
     for jentry in range(nbins):
-        #diff = amatrix[ientry][jentry] - gmatrix.GetBinContent(GetBin(ientry, jentry))
+        aval = amatrix.GetBinContent(amatrix.GetBin(ientry, jentry))
+        gval = gmatrix.GetBinContent(gmatrix.GetBin(ientry, jentry))
+        oval = aval - gval
+        omatrix.SetBinContent(omatrix.GetBin(ientry, jentry), oval)
+        if aval != 0:
+            omatrix1.SetBinContent(omatrix1.GetBin(ientry, jentry), oval/aval)
+        else:
+            omatrix1.SetBinContent(omatrix1.GetBin(ientry, jentry), 0)
         print("Matrix element: [ " + str(ientry) + " , " + str(jentry) + " ]")
-        print("\txsllhFitter output matrix entry: "+str(amatrix[ientry][jentry]))
-        print("\tGUNDAM output matrix entry: "+str(gmatrix.GetBinContent(gmatrix.GetBin(ientry, jentry))))
-        print("\tAbsolute Difference: "+str(abs(amatrix[ientry][jentry] - gmatrix.GetBinContent(gmatrix.GetBin(ientry, jentry)))))
-'''
-if not os.path.exists(apath):
-    opath = input("Enter ouput file: ")
+        print("\txsllhFitter output matrix entry: "+str(aval))
+        print("\tGUNDAM output matrix entry: "+str(gval))
+        print("\tAbsolute Difference: "+str(abs(oval)))
+        if aval != 0:
+            print("\tRelative Difference normalized with Kenji's output: "+str(oval/aval))
+        else:
+            print("\tRelative Difference normalized with Kenji's output:"+str(0))
 
-with uproot.create(opath) as file:
-    file["kenji_postfit_cov" ] = uproot.newtree({"kenji_postfit_cov" : "TMatrixD"})
-    file["gundam_postfit_cov"] = uproot.newtree({"gundam_postfit_cov": "TMatrixD"})
-    file["diff_cov"          ] = uproot.newtree({"diff_cov"          : "TMatrixD"})
 
-    file["kenji_postfit_cov" ].extend({"kenji_postfit_cov" : amatrix})
-    file["gundam_postfit_cov"].extend({"gundam_postfit_cov": gmatrix})
-    file["diff_cov"          ].extend({"diff_cov"          : omatrix})
+#print(omatrix)
+
+if not os.path.exists("per_margherita"):
+    #opath = input("Enter ouput file: ")
+    os.makedirs("per_margherita")
+
+# save to file
+ofile = TFile(opath, "RECREATE")
+amatrix.Write("kenji_postfit_cov")
+gmatrix.Write("gundam_postfit_cov")
+omatrix.Write()
+omatrix1.Write()
+ofile.Close()
+
+# Draw and save to pdf
+gStyle.SetOptStat(0)
+gStyle.SetLegendTextSize(0.03)
+c1 = TCanvas()
+omatrix.Draw("colz")
+c1.SaveAs("per_margherita/diff_cov.pdf")
+omatrix1.Draw("colz")
+c1.SaveAs("per_margherita/reldiff_cov.pdf")
